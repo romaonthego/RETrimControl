f23e5288ce2a97cd5d11aba0d71c89cd642bf29e
Roman Efimov <romaonthego@gmail.com>
2013-01-18 10:10:18 -0600

    Link window with root view controller

--- RETrimControlExample/RETrimControlExample/AppDelegate.m
+++ RETrimControlExample/RETrimControlExample/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "RootViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    self.window.rootViewController = [[RootViewController alloc] init];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
