22c14de025c2e59d7f310abe53113ba9ae5e53d0
Roman Efimov <romaonthego@gmail.com>
2013-01-18 10:03:57 -0600

    Add example project template

---
+++ RETrimControlExample/RETrimControlExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RETrimControlExample
+//
+//  Created by Roman Efimov on 1/18/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RETrimControlExample/RETrimControlExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RETrimControlExample
+//
+//  Created by Roman Efimov on 1/18/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
